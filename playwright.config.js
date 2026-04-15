9fd884265dff474c3b562b71b35c574291675df6
Manisha Bando <manishabando95@gmail.com>
2026-04-15 18:01:17 +0530

    Save local changes

--- playwright.config.js
+++ playwright.config.js
@@ -5,7 +5,9 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
+// import dotenv from 'dotenv';git fetch origin
+git pull --rebase origin main
+git push origin main
 // import path from 'path';
 
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
